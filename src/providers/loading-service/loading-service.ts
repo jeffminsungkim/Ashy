4edef921f9d9529562c6a4534d823003a626a8b5
JeffMinsungKim <ziznzkak@gmail.com>
2017-11-29 04:24:46 +0900

    Changed some loading service methods

--- src/providers/loading-service/loading-service.ts
+++ src/providers/loading-service/loading-service.ts
@@ -4,15 +4,20 @@ import { LoadingController } from 'ionic-angular';
 @Injectable()
 export class LoadingServiceProvider {
   loader: any;
+
   constructor(private loadingCtrl: LoadingController) { }
 
-  show(message: string) {
-    this.loader = this.loadingCtrl.create({ content: message});
-    return this.loader.present();
+  show(message: string) { this.loadingCtrl.create({ content: message}).present(); }
+
+  showWaitLoader() {
+    this.loader = this.loadingCtrl.create({ content: 'Please wait...'});
+    this.loader.present();
+
+    setTimeout(() => {
+      this.loader.dismiss();
+    }, 3000);
   }
 
-  dismiss() {
-    return this.loader.dismiss();
-  }
+  dismiss() { return this.loader.dismiss(); }
 
 }
