88c8b725aae10b9dc283a4f1ef01d510700bcb02
JeffMinsungKim <ziznzkak@gmail.com>
2017-11-29 04:23:41 +0900

    Added a new method to remind users that all fields are required

--- src/providers/toast-service/toast-service.ts
+++ src/providers/toast-service/toast-service.ts
@@ -3,12 +3,22 @@ import { ToastController } from 'ionic-angular';
 
 @Injectable()
 export class ToastServiceProvider {
+  fieldRequiredMessage: string;
 
-  constructor(private toastCtrl: ToastController) { }
+  constructor(private toastCtrl: ToastController) {
+    this.fieldRequiredMessage = 'All fields are required.';
+  }
 
   show(message: string, duration: number = 3000) {
     return this.toastCtrl.create({
       message,
+      duration,
+    }).present();
+  }
+
+  allFieldsRequired(duration: number = 3000) {
+    return this.toastCtrl.create({
+      message: this.fieldRequiredMessage,
       duration,
     }).present();
   }
