caed4811ec2bf1df038f076a48c1dd9eb815c0b1
JeffMinsungKim <ziznzkak@gmail.com>
2017-12-01 22:25:38 +0900

    Added observer on the Auth object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,28 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import * as firebase from 'firebase/app';
+import { environment } from '../environments/environment'
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:string = 'LoginPage';
+  rootPage:string;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    firebase.initializeApp(environment.firebase);
+
+    const unsubscribe = firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.rootPage = 'HomePage';
+        unsubscribe();
+      } else {
+        this.rootPage = 'LoginPage';
+        unsubscribe();
+      }
+    });
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
