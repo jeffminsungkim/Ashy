0d33f6a7ec6a066b3dac2dc439da19ba50e5041f
JeffMinsungKim <ziznzkak@gmail.com>
2017-12-06 20:37:56 +0900

    Added config directory for font-awesome

---
+++ config/sass.config.js
@@ -0,0 +1,13 @@
+ /**
+   * includePaths: Used by node-sass for additional
+   * paths to search for sass imports by just name.
+*/
+// Adding Font Awesome to includePaths
+module.exports = {
+  includePaths: [
+    'node_modules/ionic-angular/themes',
+    'node_modules/ionicons/dist/scss',
+    'node_modules/ionic-angular/fonts',
+    'node_modules/font-awesome/scss'
+  ]
+};
