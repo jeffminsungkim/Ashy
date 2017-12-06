6421a3c5bc8e5367ab11264024f0e4b3f619bf2b
JeffMinsungKim <ziznzkak@gmail.com>
2017-12-07 04:59:44 +0900

    Replaced page life cycle hook from ionic to angular

--- src/pages/profile/profile.module.ts
+++ src/pages/profile/profile.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
+import { NgProgressModule } from 'ngx-progressbar';
 import { ProfilePage } from './profile';
+
 
 @NgModule({
   declarations: [
     ProfilePage,
   ],
   imports: [
+    NgProgressModule,
     IonicPageModule.forChild(ProfilePage),
   ],
 })
--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, Events } from 'ionic-angular';
+import { Component, OnDestroy } from '@angular/core';
+import { IonicPage, NavController, NavParams, ActionSheetController } from 'ionic-angular';
+
+import { NativePageTransitions, NativeTransitionOptions } from '@ionic-native/native-page-transitions';
+
+import { NgProgress } from 'ngx-progressbar';
 
 import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
 import { UserServiceProvider } from '../../providers/user-service/user-service';
 import { AlertServiceProvider } from '../../providers/alert-service/alert-service';
 import { UploadServiceProvider } from '../../providers/upload-service/upload-service';
+import { ModalServiceProvider } from '../../providers/modal-service/modal-service';
 
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
@@ -17,27 +22,22 @@ import { Upload } from '../../models/upload';
   selector: 'page-profile',
   templateUrl: 'profile.html',
 })
-export class ProfilePage {
+export class ProfilePage implements OnDestroy {
   private subscription: Subscription;
   private avatar: string;
   private displayName: string;
-  private user$: Observable<any>;
-  private emailVerified: boolean;
-  private isUrlEqualsToPlaceholder: boolean;
-  private selectedFile: FileList;
-  private currentUpload: Upload;
   private previewImage: any;
-  private user: any;
 
   constructor(
     public navParams: NavParams,
     public navCtrl: NavController,
-    public events: Events,
+    private nativePageTransitions: NativePageTransitions,
+    private ngProgress: NgProgress,
     private authService: AuthServiceProvider,
     private userService: UserServiceProvider,
     private alertService: AlertServiceProvider,
-    private uploadService: UploadServiceProvider) {
-    this.emailVerified = navParams.get('emailVerified');
+    private uploadService: UploadServiceProvider,
+    private modalService: ModalServiceProvider) {
   }
 
   ionViewDidLoad() {
@@ -48,56 +48,26 @@ export class ProfilePage {
   }
 
   ionViewWillEnter() {
-    console.log("Profile Page WillEnter()");
-    this.subscription = this.userService.getLoggedInUser().subscribe((currentUser: any) => {
-      for (let user of currentUser) {
-        this.avatar = user.photoURL;
-        this.displayName = user.displayName;
-      }
+    this.subscription = this.userService.getCurrentUser().subscribe((user: any) => {
+      this.avatar = user.photoURL;
+      this.displayName = user.displayName;
     });
   }
 
-  ionViewDidLeave() {
-    console.log("Profile Page DidLeave()");
-    // this.subscription.unsubscribe();
-  }
-
-  ionViewWillLeave() {
-    console.log('Runs when the page is about to leave and no longer be the active page.');
-    this.subscription.unsubscribe();
-  }
-
-  detectFiles(event) {
-   this.selectedFile = event.target.files;
-   if (this.selectedFile) {
-     let reader = new FileReader();
-     reader.onload = (event: any) => {
-       this.previewImage = event.target.result;
-        console.log("result:",this.previewImage );
-     }
-     reader.readAsDataURL(event.target.files[0]);
-   }
- }
-
-  deleteUploadFile() {
-    if (this.selectedFile) {
-      this.previewImage = this.avatar;
-    } else {
-      this.uploadService.deleteUpload();
+  ngOnDestroy() {
+    if (this.subscription !== undefined) {
+      this.subscription.unsubscribe();
+      console.log("Profile ngOnDestroy");
     }
-    
   }
 
-  uploadSingleFile() {
-    let file = this.selectedFile.item(0);
-    this.currentUpload = new Upload(file);
-    this.isUrlEqualsToPlaceholder = false;
-    this.uploadService.pushUpload(this.currentUpload);
-  }
+  showOriginalImage() {
+    console.log("showOriginalImage()");
 
-  requestEmailVerification() {
-    this.authService.sendEmailVerification();
-    this.alertService.notifyVerificationEmailIsSent();
+  }
+  
+  editProfile() {
+    this.modalService.showProfileDetailModal();
   }
 
   backToPreviousView() {
