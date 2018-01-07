e273cc5feaaaef6eec8a4eadffa2b48f3cd51869
JeffMinsungKim <ziznzkak@gmail.com>
2018-01-07 22:06:02 +0900

    Changed missing relative paths to absolute

--- src/pages/welcome/welcome.module.ts
+++ src/pages/welcome/welcome.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-import { WelcomePage } from './welcome';
+import { WelcomePage } from '@ashy-pages/welcome/welcome';
 
 @NgModule({
   declarations: [
