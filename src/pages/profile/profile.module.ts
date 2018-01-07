921bc23a3c62b33727bf0ad0818e613f211f6f5f
JeffMinsungKim <ziznzkak@gmail.com>
2018-01-07 22:14:34 +0900

    Added missing service provider

--- src/pages/profile/profile.module.ts
+++ src/pages/profile/profile.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ProfilePage } from '@ashy-pages/profile/profile';
+import { ChatServiceProvider } from '@ashy-services/chat-service/chat-service';
 import { UploadServiceProvider } from '@ashy-services/upload-service/upload-service';
 
 @NgModule({
@@ -11,6 +12,7 @@ import { UploadServiceProvider } from '@ashy-services/upload-service/upload-serv
     IonicPageModule.forChild(ProfilePage),
   ],
   providers: [
+    ChatServiceProvider,
     UploadServiceProvider
   ]
 })
