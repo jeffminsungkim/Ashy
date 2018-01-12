8d837273e77d044af630c0d8f0a16cba8569d191
JeffMinsungKim <ziznzkak@gmail.com>
2018-01-12 17:24:57 +0900

    Organizing Firebase Cloud Functions

--- functions/index.js
+++ functions/index.js
@@ -1,27 +1,31 @@
+'use strict';
+/** EXPORT ALL FUNCTIONS
+*
+*  Loads all `.f.js` files
+*  Exports a cloud function matching the file name
+*  Author: David King
+*  Edited: Tarik Huber
+*  Based on this thread:
+*  https://github.com/firebase/functions-samples/issues/170
+*/
+
+/**
+ *  IMPORTANT!
+ *  Recommend that treat your index.js file your main file.
+ *  The main file is very simple boilerplate that composes very, very rigorously tested libraries.
+*/
+
+const glob = require("glob");
+const camelCase = require("camelcase");
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
+try { admin.initializeApp(functions.config().firebase); } catch(e) {} // You do that because the admin SDK can only be initialized once.
 const firestore = admin.firestore();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
- console.log('Hello World Test Cloud Function Triggered!');
-});
+const files = glob.sync('./**/*.f.js', { cwd: __dirname, ignore: './node_modules/**'});
+for(let f = 0,fl = files.length; f < fl; f++){
+  const file = files[f];
+  const functionName = camelCase(file.slice(0, -5).split('/').join('_')); // Strip off '.f.js'
+  if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === functionName) exports[functionName] = require(file);
+}
 
-exports.onUserStatusChanged = functions.database
-  .ref('/status/{uid}').onUpdate((event) => {
-    // Get the data written to Realtime Database
-    const eventStatus = event.data.val();
-
-    const userStatusFirestoreRef = firestore.doc(`users/${event.params.uid}`);
-
-    return event.data.ref.once("value").then((statusSnapshot) => {
-      return statusSnapshot.val();
-    }).then((status) => {
-        console.log('eventStatus:', eventStatus);
-        if (status.currentActiveStatus === 'offline' || status.currentActiveStatus === 'signout') return userStatusFirestoreRef.update(eventStatus);
-    });
-  });
