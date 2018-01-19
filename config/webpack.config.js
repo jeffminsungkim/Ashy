723b9bc64f9c7b40b5709746d6f315dd33013084
JeffMinsungKim <ziznzkak@gmail.com>
2018-01-19 14:19:52 +0900

    A major changes custom webpack config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,40 +1,53 @@
+const chalk = require("chalk");
+const fs = require('fs');
 const path = require('path');
-const webpackDefault = require('@ionic/app-scripts/config/webpack.config');
-const webpackMerge = require('webpack-merge');
+const useDefaultConfig = require('@ionic/app-scripts/config/webpack.config.js');
 
-const customConfig = {
-  dev: {
-    resolve: {
-      alias: {
-        '@ashy-app': path.resolve('src/app'),
-        '@ashy-components': path.resolve('src/components'),
-        '@ashy-core': path.resolve('src/core'),
-        '@ashy-environments': path.resolve('src/environments'),
-        '@ashy-mocks': path.resolve('src/mocks'),
-        '@ashy-models': path.resolve('src/models'),
-        '@ashy-pages': path.resolve('src/pages'),
-        '@ashy-pipes': path.resolve('src/pipes'),
-        '@ashy-services': path.resolve('src/providers'),
-        '@ashy-shared': path.resolve('src/shared')
-      }
-    }
-  },
-  prod: {
-    resolve: {
-      alias: {
-        '@ashy-app': path.resolve('src/app'),
-        '@ashy-components': path.resolve('src/components'),
-        '@ashy-core': path.resolve('src/core'),
-        '@ashy-environments': path.resolve('src/environments'),
-        '@ashy-mocks': path.resolve('src/mocks'),
-        '@ashy-models': path.resolve('src/models'),
-        '@ashy-pages': path.resolve('src/pages'),
-        '@ashy-pipes': path.resolve('src/pipes'),
-        '@ashy-services': path.resolve('src/providers'),
-        '@ashy-shared': path.resolve('src/shared')
-      }
-    }
-  }
+let env = process.env.IONIC_ENV;
+
+useDefaultConfig.prod.resolve.alias = {
+  "@ashy-app": path.resolve('src/app'),
+  "@ashy-components": path.resolve('src/components'),
+  "@ashy-core": path.resolve('src/core'),
+  "@ashy-env": path.resolve(environmentPath('prod')),
+  "@ashy-mocks": path.resolve('src/mocks'),
+  "@ashy-models": path.resolve('src/models'),
+  "@ashy-pages": path.resolve('src/pages'),
+  "@ashy-pipes": path.resolve('src/pipes'),
+  "@ashy-services": path.resolve('src/providers'),
+  "@ashy-shared": path.resolve('src/shared')
 };
 
-module.exports = webpackMerge(webpackDefault, customConfig);
+useDefaultConfig.dev.resolve.alias = {
+  "@ashy-app": path.resolve('src/app'),
+  "@ashy-components": path.resolve('src/components'),
+  "@ashy-core": path.resolve('src/core'),
+  "@ashy-env": path.resolve(environmentPath('dev')),
+  "@ashy-mocks": path.resolve('src/mocks'),
+  "@ashy-models": path.resolve('src/models'),
+  "@ashy-pages": path.resolve('src/pages'),
+  "@ashy-pipes": path.resolve('src/pipes'),
+  "@ashy-services": path.resolve('src/providers'),
+  "@ashy-shared": path.resolve('src/shared')
+};
+
+if (env !== 'prod' && env !== 'dev') {
+  // Default to dev config
+  useDefaultConfig[env] = useDefaultConfig.dev;
+  useDefaultConfig[env].resolve.alias = {
+    "@ashy-env": path.resolve(environmentPath(env))
+  };
+}
+
+function environmentPath(env) {
+  var filePath = './src/environments/environment' + (env === 'prod' ? '' : '.' + env) + '.ts';
+  if (!fs.existsSync(filePath)) {
+    console.log(chalk.red('\n' + filePath + ' does not exist!'));
+  } else {
+    return filePath;
+  }
+}
+
+module.exports = function () {
+  return useDefaultConfig;
+};
