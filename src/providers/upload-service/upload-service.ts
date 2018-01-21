237e4d23414b412191e77307f368c75b32cc77c5
JeffMinsungKim <ziznzkak@gmail.com>
2018-01-22 00:38:15 +0900

    Remove unused code

--- src/providers/upload-service/upload-service.ts
+++ src/providers/upload-service/upload-service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { UtilityServiceProvider } from '@ashy-services/utility-service/utility-service';
 import { Upload } from '@ashy-models/upload';
 import * as firebase from 'firebase/app';
 import 'firebase/storage';
@@ -7,11 +8,11 @@ declare var window: any;
 
 @Injectable()
 export class UploadServiceProvider {
-  rootDir: string = 'profile-pictures/';
+  rootDir: string = 'profile-pictures';
   uploadTask: firebase.storage.UploadTask;
   nativePath: any;
 
-  constructor() {}
+  constructor(private utilityService: UtilityServiceProvider) {}
 
   // private generateUUID(): string {
   //   function s4() {
@@ -44,8 +45,8 @@ export class UploadServiceProvider {
 
   uploadImageToCurrentUserDir(imageBlob, userDir: string) {
     return new Promise((resolve, reject) => {
-      let storageRef = firebase.storage().ref();
-       this.uploadTask = storageRef.child(`${this.rootDir}${userDir}/${imageBlob}`).put(imageBlob);
+      const storageRef = firebase.storage().ref();
+       this.uploadTask = storageRef.child(`${this.rootDir}/${userDir}/${this.utilityService.generateRandomString()}`).put(imageBlob);
         this.uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, (snapshot: any) => {
           console.log('snapshot:', snapshot);
         }, (error) => {
@@ -56,60 +57,9 @@ export class UploadServiceProvider {
       });
   }
 
-  // saveFile(image) {
-  //   let ref = this.afDB.object(`${this.rootNode}/${this.userService.currentUserId}`);
-  //   return new Promise((resolve, reject) => {
-  //     let data = {
-  //       'url': image.downloadURL,
-  //       'uid': image.metadata.name,
-  //       'email': this.userService.currentUserEmail,
-  //       'lastUpdated': new Date().getTime()
-  //     };
-  //     ref.update(data).then(() => {
-  //       this.userService.updatePhotoUrlFromPlaceholder(image.downloadURL);
-  //     }).catch(err => reject(err));
-  //   });
-  // }
-
-
-  // pushUpload(upload: Upload) {
-  //   let storageRef = firebase.storage().ref();
-  //   this.uploadTask = storageRef.child(`${this.rootNode}/${this.userService.currentUserId}`).put(upload.file);
-  //   this.uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
-  //     (snapshot: any) => {
-  //       upload.progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-  //       console.log("PROGRESS:", upload.progress);
-  //     }, (error) => {
-  //       console.log("UPLOAD ERROR:", error)
-  //     }, () => {
-  //       upload.url = this.uploadTask.snapshot.downloadURL
-  //       upload.name = upload.file.name
-  //     });
-  // }
-
-  // private saveFileNode(upload: Upload) {
-  //   this.afDB.object(`${this.rootNode}/${this.userService.currentUserId}`).update(upload)
-  //   .then(() => {
-  //     this.userService.updatePhotoUrlFromPlaceholder(upload.url);
-  //   });
-  // }
-
-  // deleteUpload() {
-  //   this.userService.updatePhotoUrlToPlaceholder();
-  //   this.deleteFileNode().then(() => {
-  //     this.deleteFileStorage();
-  //   })
-  //   .catch(error => console.log("DELETE FILE STORAGE ERROR:", error))
-  // }
-
   deleteFileStorage(uid: string) {
     const storageRef = firebase.storage().ref();
-    storageRef.child(`${this.rootDir}${uid}`).delete();
+    storageRef.child(`${this.rootDir}/${uid}`).delete();
   }
-
-//   deleteFileNode() {
-//     let uid = this.userService.currentUserId;
-//     return this.afDB.list(`${this.rootNode}`).remove(uid);
-//  }
 
 }
