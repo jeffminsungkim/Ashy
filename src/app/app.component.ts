0090024d7bdde195e19ef2fd08cc37444d0bf95c
JeffMinsungKim <ziznzkak@gmail.com>
2018-01-21 20:06:23 +0900

    Direct user to email verification page when displayname is null

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,7 +29,7 @@ export class MyApp {
     platform.ready().then((readySource) => {
       afAuth.auth.onAuthStateChanged(user => {
         console.log("App User", user);
-        if (user && user.emailVerified) {
+        if (user && user.emailVerified && user.displayName) {
           userService.getUserStatus().once('value').then((snapshot) => {
             const status = snapshot.val().currentActiveStatus;
             const userSignedIn = snapshot.val().usingApp;
@@ -40,21 +40,24 @@ export class MyApp {
             }
           });
           this.rootPage = 'HomePage';
-        } else {
-          this.rootPage = 'LoginPage';
+        }
+        else if (user && !user.emailVerified || user && !user.displayName) {
+          this.rootPage = 'EmailVerificationPage';
+        }
+        else {
+          this.rootPage = 'LoginPage'; // TODO: Switch Weclome page
         }
       });
 
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       console.log('Platform ready from', readySource);
+      keyboard.disableScroll(true);
       statusBar.styleDefault();
       splashScreen.hide();
       imageLoaderConfig.enableSpinner(false);
       imageLoaderConfig.useImageTag(true);
       imageLoaderConfig.setMaximumCacheSize(20 * 1024 * 1024); // set max size to 20MB
-      if (platform.is('ios'))
-        keyboard.disableScroll(true);
     });
   }
 }
