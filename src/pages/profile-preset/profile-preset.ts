7ab7332b13ffa906d85d0a6c98408a246a16afc0
JeffMinsungKim <ziznzkak@gmail.com>
2018-01-21 23:50:31 +0900

    Added a feature to upload profile picture

--- src/pages/profile-preset/profile-preset.ts
+++ src/pages/profile-preset/profile-preset.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { Http } from '@angular/http';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ActionSheetController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 import { AuthServiceProvider } from '@ashy-services/auth-service/auth-service';
+import { LoadingServiceProvider } from '@ashy-services/loading-service/loading-service';
 import { ToastServiceProvider } from '@ashy-services/toast-service/toast-service';
 import { UserServiceProvider } from '@ashy-services/user-service/user-service';
+import { UploadServiceProvider } from '@ashy-services/upload-service/upload-service';
 
 
 @IonicPage()
@@ -12,15 +14,30 @@ import { UserServiceProvider } from '@ashy-services/user-service/user-service';
   templateUrl: 'profile-preset.html',
 })
 export class ProfilePresetPage {
+
+  public avatar: string;
   public displayName: string;
+  public placeholder: string = 'assets/avatar/placeholder.jpg';
+
+  cameraOptions: CameraOptions = {
+    quality: 100,
+    correctOrientation: true,
+    sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+    destinationType: this.camera.DestinationType.FILE_URI,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+  };
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public http: Http,
+    public actionSheetCtrl: ActionSheetController,
+    public camera: Camera,
     private authService: AuthServiceProvider,
+    private  loadingService: LoadingServiceProvider,
     private toastService: ToastServiceProvider,
-    private userService: UserServiceProvider) {
+    private userService: UserServiceProvider,
+    private uploadService: UploadServiceProvider) {
 
   }
 
@@ -29,7 +46,49 @@ export class ProfilePresetPage {
   }
 
   uploadProfilePicture() {
+    this.camera.getPicture(this.cameraOptions).then((imagePath) => {
+      this.loadingService.show('Please wait');
+      console.log("IMAGE PATH", imagePath);
+      return this.uploadService.convertImageIntoBlob(imagePath);
+    }).then((imageBlob) => {
+      console.log("IMAGE BLOB", imageBlob);
+      return this.uploadService.uploadImageToCurrentUserDir(imageBlob, this.userService.currentUserId);
+    }).then((snapshot: any) => {
+      this.avatar = snapshot.downloadURL;
+      console.log("FILE UPLOAD SUCCESSFULLY", snapshot.downloadURL);
+      this.placeholder = null;
+      this.loadingService.dismiss();
+    });
+  }
 
+  deleteUploadFile() {
+
+  }
+  presentActionSheet() {
+    let actionSheet = this.actionSheetCtrl.create({
+      cssClass:'alert-buttons',
+      buttons: [
+        {
+          text: 'Photo from library',
+          handler: () => {
+            this.uploadProfilePicture();
+          }
+        },
+        {
+          text: 'Remove Profile Photo',
+          role: 'destructive',
+          cssClass: 'remove-button',
+          handler: () => {
+            this.deleteUploadFile();
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    });
+    actionSheet.present();
   }
 
   startApp() {
