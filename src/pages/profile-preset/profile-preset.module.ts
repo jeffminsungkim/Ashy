f72269383729dd69776f6cfb925c28496411a7ab
JeffMinsungKim <ziznzkak@gmail.com>
2018-01-21 23:50:45 +0900

    Added upload service provider

--- src/pages/profile-preset/profile-preset.module.ts
+++ src/pages/profile-preset/profile-preset.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ProfilePresetPage } from './profile-preset';
+import { UploadServiceProvider } from '@ashy-services/upload-service/upload-service';
+
 
 @NgModule({
   declarations: [
@@ -9,5 +11,8 @@ import { ProfilePresetPage } from './profile-preset';
   imports: [
     IonicPageModule.forChild(ProfilePresetPage),
   ],
+  providers: [
+    UploadServiceProvider
+  ]
 })
 export class ProfilePresetPageModule {}
