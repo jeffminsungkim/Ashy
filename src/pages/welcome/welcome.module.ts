cfd8dd30b60e940834446604669c143a147eb4da
JeffMinsungKim <ziznzkak@gmail.com>
2018-01-28 00:54:04 +0900

    Added twitter login feature

--- src/pages/welcome/welcome.module.ts
+++ src/pages/welcome/welcome.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { WelcomePage } from '@ashy-pages/welcome/welcome';
+import { TwitterConnect } from '@ionic-native/twitter-connect';
 
 @NgModule({
   declarations: [
@@ -9,5 +10,6 @@ import { WelcomePage } from '@ashy-pages/welcome/welcome';
   imports: [
     IonicPageModule.forChild(WelcomePage),
   ],
+  providers: [TwitterConnect]
 })
 export class WelcomePageModule {}
--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { TwitterConnect } from '@ionic-native/twitter-connect';
 
+import { AuthServiceProvider } from "@ashy-services/auth-service/auth-service";
+import { LoadingServiceProvider } from '@ashy-services/loading-service/loading-service';
+import { UserServiceProvider } from '@ashy-services/user-service/user-service';
 
 @IonicPage()
 @Component({
@@ -9,7 +13,28 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class WelcomePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private authService: AuthServiceProvider,
+    private loadingService: LoadingServiceProvider,
+    private twitterConnect: TwitterConnect,
+    private userService: UserServiceProvider) {
+  }
+
+  loginWithTwitter() {
+    this.twitterConnect.login().then((res) => {
+      this.loadingService.showAuthenticationLoader();
+      console.log('token:',res.token);
+      console.log('secret:',res.secret);
+      this.authService.twitterLogin(res.token, res.secret).then(user => {
+        console.log('user test:', user);
+        this.userService.updateCurrentUserActiveStatusTo('online');
+        this.userService.updateCurrentUserAppUsageStatusTo(true, 'signout');
+        this.navCtrl.setRoot('HomePage');
+        this.loadingService.dismiss();
+      }).catch(error => this.loadingService.dismiss());
+    });
   }
 
   goToRegisterPage() {
