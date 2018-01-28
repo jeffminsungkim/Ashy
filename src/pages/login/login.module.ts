7cee54855955173bef317a015cd177248c83075a
JeffMinsungKim <ziznzkak@gmail.com>
2018-01-28 23:50:02 +0900

    Removed module

--- src/pages/login/login.module.ts
+++ src/pages/login/login.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { LoginPage } from '@ashy-pages/login/login';
 
+
 @NgModule({
   declarations: [
     LoginPage,
