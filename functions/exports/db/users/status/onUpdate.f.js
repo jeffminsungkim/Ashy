e9d0acfee32be237e04dcc9fe02a54f41cf579a3
JeffMinsungKim <ziznzkak@gmail.com>
2018-02-03 00:24:44 +0900

    Added current user email for log

--- functions/exports/db/users/status/onUpdate.f.js
+++ functions/exports/db/users/status/onUpdate.f.js
@@ -4,13 +4,13 @@ module.exports = ({ functions, firestore }) => {
   return functions.database.ref('/status/{uid}').onUpdate((event) => {
     // Get the data written to Realtime Database
     const eventStatus = event.data.val();
-
     const userStatusFirestoreRef = firestore.doc(`users/${event.params.uid}`);
+    const currentUserEmail = event.auth.variable.email;
 
     return event.data.ref.once('value').then((statusSnapshot) => {
       return statusSnapshot.val();
     }).then((status) => {
-        console.log('eventStatus:', eventStatus);
+        console.log(currentUserEmail, ':', eventStatus);
         if (status.currentActiveStatus === 'offline' || status.currentActiveStatus === 'signout')
           return userStatusFirestoreRef.update({currentActiveStatus: eventStatus.currentActiveStatus});
     });
