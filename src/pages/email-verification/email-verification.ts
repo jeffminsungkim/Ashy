b31081cb3075261cb94dc2546fc19767ec7512b9
JeffMinsungKim <ziznzkak@gmail.com>
2018-02-03 15:29:49 +0900

    Removed upload model

--- src/pages/email-verification/email-verification.ts
+++ src/pages/email-verification/email-verification.ts
@@ -1,4 +1,3 @@
-import { Upload } from './../../models/Upload';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
