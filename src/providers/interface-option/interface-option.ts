ab69b5a2350cf90fd6970f0eb1bf2f34922c152a
JeffMinsungKim <ziznzkak@gmail.com>
2018-02-13 23:01:08 +0900

    Created a new service to handle interface options

---
+++ src/providers/interface-option/interface-option.ts
@@ -0,0 +1,100 @@
+import { Injectable } from '@angular/core';
+
+
+@Injectable()
+export class InterfaceOption {
+
+  svgPath: string = 'assets/svgs/manhatten-pastel-red-200.svg';
+
+  constructor() {
+
+  }
+
+  /**
+   * Alert Controller Options
+   */
+
+  makeErrorMessageOpt(message: string) {
+    return {
+      title: 'Be careful!',
+      message: message,
+      buttons: [
+        {
+          text: 'OK',
+          role: 'cancel'
+        }
+      ]
+    }
+  }
+
+  makeEmailVerificationOpt() {
+    return {
+      title: 'Email has sent!',
+      subTitle: 'Please wait this might take a few minutes.',
+      message: 'Try it again if you have not received an email.',
+      buttons: [
+        {
+          text: 'OK',
+          role: 'cancel'
+        }
+      ]
+    }
+  }
+
+  makePasswordResetOpt() {
+    return {
+      title: 'Verification Email has sent!',
+      subTitle: 'Please confirm the verification link from your email account. This might take a few minutes.',
+      buttons: [
+        {
+          text: 'OK',
+          role: 'cancel'
+        }
+      ]
+    }
+  }
+
+  makeConfirmPasswordResetEmail() {
+    return {
+      title: 'Send Password Reset Email?',
+      message: 'Please confirm the verification link from your email account. This might take a few minutes.',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Send',
+          handler: () => {
+          }
+        }
+      ]
+    }
+  }
+
+  /**
+   * Loading Controller Options
+   */
+
+  makeWaitLoaderOpt() {
+    return {
+      spinner: 'hide',
+      content: `<img src=${this.svgPath}>`
+    }
+  }
+
+
+  /**
+   * Toast Controller Options
+   */
+
+  makeShowToastOpt(message: string, duration: number = 3000) {
+    return {
+      message,
+      duration
+    }
+  }
+
+
+
+}
