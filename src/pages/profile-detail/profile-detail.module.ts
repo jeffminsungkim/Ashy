372c10329362862c02a0e989bd2b97bad441275a
JeffMinsungKim <ziznzkak@gmail.com>
2018-02-22 01:25:55 +0900

    Resolved #42: Created a simple profile edit page

--- src/pages/profile-detail/profile-detail.module.ts
+++ src/pages/profile-detail/profile-detail.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
+import { RelativeTimePipe } from '@ashy/pipes/relative-time/relative-time';
 import { ProfileDetailPage } from '@ashy/pages/profile-detail/profile-detail';
 import { UploadServiceProvider } from '@ashy/services/upload-service/upload-service';
 
 @NgModule({
   declarations: [
     ProfileDetailPage,
+    RelativeTimePipe
   ],
   imports: [
     IonicPageModule.forChild(ProfileDetailPage),
   ],
   providers: [
-    UploadServiceProvider
+    UploadServiceProvider,
   ]
 })
 export class ProfileDetailPageModule {}
