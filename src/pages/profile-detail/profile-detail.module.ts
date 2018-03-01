bfdd271ae7d9423b0fdae23c374b9fc40ab5dcaa
JeffMinsungKim <ziznzkak@gmail.com>
2018-03-02 04:21:40 +0900

    Use a custom simple relative time pipe

--- src/pages/profile-detail/profile-detail.module.ts
+++ src/pages/profile-detail/profile-detail.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-import { RelativeTimePipe } from '@ashy/pipes/relative-time/relative-time';
+import { SimpleRelativeTimePipe } from '@ashy/pipes/simple-relative-time/simple-relative-time';
 import { ProfileDetailPage } from '@ashy/pages/profile-detail/profile-detail';
 import { UploadServiceProvider } from '@ashy/services/upload-service/upload-service';
 
 @NgModule({
   declarations: [
     ProfileDetailPage,
-    RelativeTimePipe
+    SimpleRelativeTimePipe
   ],
   imports: [
     IonicPageModule.forChild(ProfileDetailPage),
