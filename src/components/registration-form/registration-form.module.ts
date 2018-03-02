df1358978c17c6e3311dc5d2bc31f12b5d8d88f6
JeffMinsungKim <ziznzkak@gmail.com>
2018-03-02 22:03:28 +0900

    Removed an extra slash

--- src/components/registration-form/registration-form.module.ts
+++ src/components/registration-form/registration-form.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 import { IonicModule } from 'ionic-angular';
-import { RegistrationFormComponent } from '@ashy/components/registration-form//registration-form';
+import { RegistrationFormComponent } from '@ashy/components/registration-form/registration-form';
 
 
 @NgModule({
