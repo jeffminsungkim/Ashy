1299be21678162657a785001af8fa967f4e12e6c
JeffMinsungKim <ziznzkak@gmail.com>
2018-03-04 00:25:34 +0900

    Added a new pipe module

--- src/pipes/pipes.module.ts
+++ src/pipes/pipes.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RelativeTimePipe } from '@ashy/pipes/relative-time/relative-time';
+import { SimpleRelativeTimePipe } from '@ashy/pipes/simple-relative-time/simple-relative-time';
+import { WordcounterPipe } from '@ashy/pipes/wordcounter/wordcounter';
+
 
 @NgModule({
-	declarations: [RelativeTimePipe],
-	imports: [],
-	exports: [RelativeTimePipe]
+	declarations: [
+    RelativeTimePipe,
+    SimpleRelativeTimePipe,
+    WordcounterPipe
+  ],
+	exports: [
+    RelativeTimePipe,
+    SimpleRelativeTimePipe,
+    WordcounterPipe
+  ]
 })
 export class PipesModule {}
