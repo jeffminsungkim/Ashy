25540bbf8f0787e9c612ddfa947d212041ffe413
JeffMinsungKim <ziznzkak@gmail.com>
2018-03-09 01:02:58 +0900

    Included process loader during authentication

--- src/pages/user-re-authentication/user-re-authentication.ts
+++ src/pages/user-re-authentication/user-re-authentication.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-import { IonicPage, NavController, NavParams, Events } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Events, LoadingController } from 'ionic-angular';
 import { AuthServiceProvider } from '@ashy/services/auth-service/auth-service';
+import { InterfaceOption } from '@ashy/services/interface-option/interface-option';
 import { LocalStorageServiceProvider } from '@ashy/services/local-storage-service/local-storage-service';
 import { UserServiceProvider } from '@ashy/services/user-service/user-service';
 import { UtilityServiceProvider } from '@ashy/services/utility-service/utility-service';
@@ -26,8 +27,10 @@ export class UserReAuthenticationPage implements OnInit {
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
+    public loadingCtrl: LoadingController,
     private events: Events,
     private authService: AuthServiceProvider,
+    private interfaceOpt: InterfaceOption,
     private localStorage: LocalStorageServiceProvider,
     private userService: UserServiceProvider,
     private utilityService: UtilityServiceProvider) {
@@ -106,16 +109,24 @@ export class UserReAuthenticationPage implements OnInit {
   }
 
   reAuthenticateWithCredential(password: string) {
+    let loader = this.loadingCtrl.create(this.interfaceOpt.makeWaitLoaderOpt());
+    loader.present();
     this.authService.reAuthenticate(password).then(() => {
       if (this.emailOrPasswd === 'email') {
         this.authService.updateEmailAddress(this.currentEmail).then(() => {
           this.adjustIdenticonHash();
           this.userService.updateUserEmailAddress(this.currentEmail).then(() => {
+            // TODO: Write a method to update identicon image to storage
+            loader.dismiss();
             this.backToPreviousPage();
           });
-        }).catch((err) => console.log('Sending verification link fail', err));
+        }).catch((err) => {
+          loader.dismiss();
+          console.log('Sending verification link fail', err);
+        });
       }
       else {
+        loader.dismiss();
         this.navCtrl.push('PasswordResetPage', { currentPassword: password });
       }
     })
