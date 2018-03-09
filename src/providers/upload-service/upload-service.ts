658996da3fe4e38effe4b209480b93f6a9650881
JeffMinsungKim <ziznzkak@gmail.com>
2018-03-10 01:05:00 +0900

    fix(service): Add uploading image functions

--- src/providers/upload-service/upload-service.ts
+++ src/providers/upload-service/upload-service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 import { UtilityServiceProvider } from '@ashy/services/utility-service/utility-service';
 import { Upload } from '@ashy/models/upload';
 import * as firebase from 'firebase/app';
@@ -11,10 +12,39 @@ export class UploadServiceProvider {
   rootDir: string = 'user-profile';
   uploadTask: firebase.storage.UploadTask;
   nativePath: any;
+  cameraOptions: CameraOptions = {
+    quality: 100,
+    correctOrientation: true,
+    sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+    destinationType: this.camera.DestinationType.FILE_URI,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+  };
 
-  constructor(private utilityService: UtilityServiceProvider) {}
+  constructor(private camera: Camera, private utilityService: UtilityServiceProvider) {}
 
-  convertImageIntoBlob(imagePath: string) {
+  getDownloadUrlFromPhotoLibrary(uid: string) {
+    return new Promise((resolve, reject) => {
+      this.camera.getPicture(this.cameraOptions)
+      .then((imagePath) => {
+        console.log("Image Path", imagePath);
+        return this.convertFileImageIntoBlob(imagePath);
+      })
+      .then((imageBlob) => {
+        console.log("Blob", imageBlob);
+        return this.uploadImageToStorage(imageBlob, uid);
+      })
+      .then((snapshot: any) => {
+        console.log('downloadURL:', snapshot.downloadURL);
+        resolve(snapshot.downloadURL);
+      })
+      .catch(err => {
+        reject(err);
+      });
+    });
+  }
+
+  convertFileImageIntoBlob(imagePath: string) {
     return new Promise((resolve, reject) => {
       window.resolveLocalFileSystemURL(imagePath, (fileEntry) => {
         fileEntry.file((resFile) => {
@@ -29,6 +59,21 @@ export class UploadServiceProvider {
           };
           reader.readAsArrayBuffer(resFile);
         });
+      });
+    });
+  }
+
+  uploadIdenticon(svgBlob, uid: string) {
+    return new Promise((resolve, reject) => {
+      const storageRef = firebase.storage().ref();
+      this.uploadTask = storageRef.child(`identicons/${uid}.svg`).put(svgBlob);
+      console.log('upload task:', this.uploadTask);
+      this.uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, (snapshot: any) => {
+        console.log('snapshot:', snapshot);
+      }, (error) => {
+        console.log('uploadimage error', error);
+      }, () => {
+        resolve(this.uploadTask.snapshot);
       });
     });
   }
@@ -50,7 +95,8 @@ export class UploadServiceProvider {
 
   deleteFileStorage(uid: string) {
     const storageRef = firebase.storage().ref();
-    storageRef.child(`${this.rootDir}/${uid}`).delete();
+    storageRef.child(`${this.rootDir}/${uid}/thumb_${uid}.jpeg`).delete();
+    storageRef.child(`${this.rootDir}/${uid}/${uid}.jpeg`).delete();
   }
 
 }
