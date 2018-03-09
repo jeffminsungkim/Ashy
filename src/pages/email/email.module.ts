36f92db24a08b7f04ee93f235c01a3b4b3b736ba
JeffMinsungKim <ziznzkak@gmail.com>
2018-03-10 00:24:20 +0900

    fix(page): Add email validation (#46)

--- src/pages/email/email.module.ts
+++ src/pages/email/email.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { EmailPage } from '@ashy/pages/email/email';
-
+import { ValidationServiceProvider } from '@ashy/services/validation-service/validation-service';
 
 
 @NgModule({
@@ -11,5 +11,8 @@ import { EmailPage } from '@ashy/pages/email/email';
   imports: [
     IonicPageModule.forChild(EmailPage),
   ],
+  providers: [
+    ValidationServiceProvider
+  ]
 })
 export class EmailPageModule {}
--- src/pages/email/email.ts
+++ src/pages/email/email.ts
@@ -1,7 +1,10 @@
 import { Component, ViewChild } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { IonicPage, NavController, Events } from 'ionic-angular';
+import { CustomValidator } from '@ashy/shared/custom-validator';
 import { ModalWrapperPage } from '@ashy/pages/modal-wrapper/modal-wrapper';
 import { AuthServiceProvider } from '@ashy/services/auth-service/auth-service';
+import { ValidationServiceProvider } from '@ashy/services/validation-service/validation-service';
 
 
 @IonicPage()
@@ -10,21 +13,24 @@ import { AuthServiceProvider } from '@ashy/services/auth-service/auth-service';
   templateUrl: 'email.html',
 })
 export class EmailPage {
-  public title: string;
-  public labelName: string;
-  public infoMessage: string;
-  public warningMessage: string;
-  public verifiedMessage: string;
-  public currentEmail: string;
-  public pristineEmail:string;
-  public verifiedState: boolean;
-  public showCloseBtn: boolean;
+  @ViewChild('inputBox') emailInput;
+  emailControl: FormControl;
+  title: string;
+  labelName: string;
+  infoMessage: string;
+  warningMessage: string;
+  verifiedMessage: string;
+  currentEmail: string;
+  pristineEmail:string;
+  verifiedState: boolean;
+  showCloseBtn: boolean;
 
   constructor(
     public navCtrl: NavController,
     private events: Events,
     private modalWrapper: ModalWrapperPage,
-    private authService: AuthServiceProvider) {
+    private authService: AuthServiceProvider,
+    private validationService: ValidationServiceProvider) {
 
     this.showCloseBtn = this.modalWrapper.modalParams.showCloseBtn;
     this.pristineEmail = this.authService.currentUserEmail;
@@ -36,9 +42,26 @@ export class EmailPage {
   }
 
   ionViewWillEnter() {
-    console.log('enter email page');
     this.checkEmailVerificationState();
     this.subscribeUpdatedEmail();
+  }
+
+  ionViewWillLoad() { this.createEmailForm(); }
+
+  get newEmail() { return this.emailControl; }
+
+  createEmailForm() {
+    const email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    this.emailControl = new FormControl(this.pristineEmail,
+    {
+      validators: [Validators.required, CustomValidator.inspectPattern(email)],
+      asyncValidators: CustomValidator.checkEmailAvailability(this.validationService)
+    });
+  }
+
+  clearFormError() {
+    this.newEmail.setValue(this.pristineEmail);
+    this.emailInput.setFocus();
   }
 
   noteWarningMessage(email: string) {
@@ -47,11 +70,8 @@ export class EmailPage {
 
   checkEmailVerificationState() {
     this.verifiedState = this.authService.currentUserEmailVerified;
-    if (this.verifiedState) {
-      this.verifiedMessage = 'Your email address is verified';
-    } else {
-      this.infoMessage = "We've sent a verification email to you. Please open the link to finish verifying your address.";
-    }
+    if (this.verifiedState) this.verifiedMessage = 'Your email address is verified';
+    else this.infoMessage = "We've sent a verification email to you. Please open the link to finish verifying your address.";
   }
 
   private subscribeUpdatedEmail() {
@@ -65,14 +85,26 @@ export class EmailPage {
     });
   }
 
+  renderClass() {
+    if (this.newEmail.value === this.pristineEmail) return;
+    switch(this.newEmail.status){
+      case "INVALID":
+        return "notification is-danger"
+      case "VALID":
+        return "notification is-success"
+      case "PENDING":
+        return "notification is-info"
+      default:
+        break;
+    }
+  }
+
   authenticateCredential($event) {
     if ($event.valid) this.navCtrl.push('UserReAuthenticationPage', { credential: 'Email', currentEmail: $event.value });
   }
 
   sendVerificationLink($event) {
-    this.authService.sendEmailVerification().then(() => {
-      console.log('Verification link has been sent to', $event);
-    });
+    this.authService.sendEmailVerification().then(() => console.log('Verification link has been sent to', $event));
   }
 
   dismissModal() { this.modalWrapper.dismissModal({ email: this.pristineEmail }); }
