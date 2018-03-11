31852982733fee1bf78dc70ec5f9c0ef2bf88bc7
JeffMinsungKim <ziznzkak@gmail.com>
2018-03-12 02:15:06 +0900

    refactor(service): Add a base class for REST

---
+++ src/providers/http/rest-service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { LocalStorageServiceProvider } from '@ashy/services/local-storage-service/local-storage-service';
+import { Observable } from 'rxjs/Observable';
+
+
+export abstract class RestService {
+
+  protected _baseUrl: string;
+  protected _token: string;
+
+  constructor(protected http: HttpClient, protected localStorage: LocalStorageServiceProvider) {}
+
+  protected set baseUrl(url: string) { this._baseUrl = url; }
+
+  protected set accessToken(token: string) { this._token = token; }
+
+  protected get baseUrl(): string { return this._baseUrl; }
+
+  protected get accessToken(): string { return this._token; }
+
+  protected get headers(): HttpHeaders {
+     return new HttpHeaders({'Authorization': this.accessToken, 'Content-Type': 'application/json; charset=utf-8'});
+  }
+
+  protected post(relativeUrl: string, data: any): Observable<any> {
+    const url = this.baseUrl + relativeUrl;
+    return this.http.post(url, JSON.stringify(data), { headers: this.headers });
+  }
+}
